68849307ae162b435e22a0e0bd46319b81ca3e0d
waliuddin1105 <waliahmed1105@gmail.com>
2025-06-30 12:25:51 +0500

    Updated getDetails function

--- tms.py
+++ tms.py
@@ -7,13 +7,15 @@ class customer:
         self.adress=''
         self.mobile=''
         
-    def getDtails():
+    def getDetails(self):
         cid=input('Enter Customer ID:')
+        self.cusId=cid
         n=input('Enter Customer name:')
-
+        self.name=n
         while True:
             try:
                 a=input('Enter Customer age:')
+                self.age=a
                 break
             except ValueError as v:
                 print("Please enter a number only") 
@@ -25,18 +27,23 @@ class customer:
                 if g!='f' and g!='m':
                     raise Exception
                 else:
+                    self.gender=g
                     break
 
             except Exception:
                 print("Please enter f or m only!")
 
         adr=input('Enter Customer address:')
-
+        self.adress=adr
         while True:
             try:
                 mob=int(input('Enter Customer mobile number:'))
+                self.mobile=mob
                 break
             except ValueError as v:
-                print("Please enter a number only")        
+                print("Please enter a number only")     
 
-        
+        with open('Old_customers.txt','w+') as f:
+            f.write(f'Customer ID:{self.cusId}\nCustomer Name:{self.name}\nCustomer Age:{self.age}\nGender:{self.gender}\nMobile Number:{self.mobile}')   
+
+    
